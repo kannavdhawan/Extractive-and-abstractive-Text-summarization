aa01e066d8a55c8a7ca6c4c49c0b0526fe3f9026
Kannav Dhawan <k4dhawan@uwaterloo.ca>
2020-07-08 03:13:43 +0000

    ctrl

--- baseline_nltk.py
+++ baseline_nltk.py
@@ -5,7 +5,7 @@ import re
 import nltk
 import heapq 
 import matplotlib.pyplot as plt
-import Word
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 nltk.download('punkt')
 nltk.download('stopwords')
 stop_words = nltk.corpus.stopwords.words('english')
@@ -20,13 +20,9 @@ for paragraph in soup.find_all('p'):
 text_lower= text.lower()
 clean_text = re.sub(r'\[[0-9]*\]',' ',text_lower)            
 clean_text = re.sub('[^a-zA-Z.]', ' ', clean_text )  
-clean_text = re.sub(r'\d',' ',clean_text)
+# clean_text = re.sub(r'\d',' ',clean_text)
 clean_text = re.sub(r'\s+',' ',clean_text)
 sentences = nltk.sent_tokenize(clean_text)
-# print("***************************************************************************************************************************")
-# print(sentences)
-
-
 # making the dict for word count and calculating the weighted histogram 
 word_count=dict()
 tokenized_words=nltk.word_tokenize(clean_text)
@@ -39,20 +35,30 @@ for word in tokenized_words:
 maximum=max(word_count.values())
 for word in word_count.keys():
     word_count[word]=word_count[word]/maximum
-print(word_count)
+# print(word_count)
 
 # Create and generate a word cloud image:
-wordcloud = WordCloud().generate(word_count)
+wordcloud = WordCloud().generate_from_frequencies(word_count)
 
 # Display the generated image:
 plt.imshow(wordcloud, interpolation='bilinear')
 plt.axis("off")
 plt.show()
-
-
-
 plt.savefig('word_count.png')
 
+#sentence scoring
+sent_score=dict()
+for sent in sentences:      #['I am kannav dhawan','I am kannavdhawan']
+    tokens_per_sent=nltk.word_tokenize(sent)    #['I','am','kannav','dhawan']
+    for word in tokens_per_sent: #I
+        if word in word_count.keys():#
+            if sent not in sent_score:
+                sent_score[sent]=word_count[word]
+            else:
+                sent_score[sent]+=word_count[word] #
+top_sent= heapq.nlargest(3,sent_score,key=sent_score.get)
+summary = ' '.join(top_sent) 
+print(summary)
 # url_topull = input('Enter t`he Wikipedia URL to pull - ')
 # sent_num = int(input('How many sentences long do you want your summary to be? (min recommended =10) '))
 
