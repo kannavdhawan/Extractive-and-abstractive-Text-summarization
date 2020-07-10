787e384277cff9adf355cb3406e7aed93b15571a
Kannav Dhawan <k4dhawan@uwaterloo.ca>
2020-07-10 07:13:12 +0000

    rouge

--- baseline/rouge_scores.py
+++ baseline/rouge_scores.py
@@ -27,29 +27,48 @@ def precision_unigrams(ref_sum_list,hyp_sum_list):
         recall.append(recall_temp)
     return precision,recall
 
-def rouge_fpr(ref_sum_list,gen_sum_list):
+def rouge_fpr(all_file_names,ref_summary_path,hyp_summary_path):
     files_rouge = FilesRouge()
     scores=[]
-    for ref,gen in zip(ref_sum_list,gen_sum_list):
+    for ref,hyp in zip(all_file_names,all_file_names):
+        count=0
+        if count==0:
+            topic="business/"
+        elif count==1:
+            topic="entertainment/"
+        elif count==2:
+            topic="politics/"
+        elif count==3:
+            topic="sport/"
+        elif count==4:
+            topic="tech/"
+
+
+        count+=1
         tmp=[]
-        for r,g in zip(ref,gen):
-            tmp.append(files_rouge.get_scores(r,g,avg=True))
+        for r,h in zip(ref,hyp):
+            tmp.append(files_rouge.get_scores(os.path.join(ref_summary_path,topic,r),os.path.join(hyp_summary_path,topic,h),avg=True))
         scores.append(tmp)
-        print(scores)
-        break
+        
     return scores
 
-def main(ref_summary_path,gen_summary_path):
+def main(ref_summary_path,hyp_summary_path):
     all_file_names=get_filenames(True)
     """
-    For self defined testing..
+    Self precision and recall..
     """
     ref_sum_list=read_text(all_file_names,ref_summary_path,testing_flag=True)
-    hyp_sum_list=read_text(all_file_names,gen_summary_path,testing_flag=True)
+    hyp_sum_list=read_text(all_file_names,hyp_summary_path,testing_flag=True)
 
     precision,recall=precision_unigrams(ref_sum_list,hyp_sum_list)
     print("Average precision of Business summaries: ",sum(precision[0][:])/len(precision[0][:]))
     print("Average recall of Business summaries: ",sum(recall[0][:])/len(recall[0][:]))
+
+    """
+    rouge_fpr
+    """
+    scores=rouge_fpr(all_file_names,ref_summary_path,hyp_summary_path)
+
 
 if __name__=='__main__':
     main('bbc_news_corpus/Summaries/','Nltk_summaries/')
